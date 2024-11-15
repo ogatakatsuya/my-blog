5b53809aeadedafede9bad3eb0cbee783a0249b6
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-11-16 04:31:23 +0900

    fix: scriptを変更

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import { renderer } from './renderer'
 
 const app = new Hono()
 
-app.use(renderer)
+app.get('*', renderer)
 
 app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
+  return c.render(<h1>Hello, Cloudflare Pages!</h1>)
 })
 
 export default app
